727fa0d8fa33170a4434c8ddca9df9cf2568281a
H3rN4n <desouza030689@gmail.com>
2016-12-12 16:57:34 -0300

    relations

--- common/models/group.js
+++ common/models/group.js
@@ -1,10 +1,11 @@
 'use strict';
+//var User = require('./user.js');
 
 module.exports = function(Group) {
 
-    Group.embedsMany(User, {
-        as: 'user', // default to the relation name - address
-        property: 'users' // default to addressItem
-    });
+    // Group.embedsOne(User, {
+    //     as: 'user', // default to the relation name - address
+    //     property: 'users' // default to addressItem
+    // });
 
 };
