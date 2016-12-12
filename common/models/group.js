ed9405a57df11b846b794918b62636f3fdf53625
H3rN4n <desouza030689@gmail.com>
2016-12-12 15:13:12 -0300

    wip - users and groups

--- common/models/group.js
+++ common/models/group.js
@@ -2,4 +2,9 @@
 
 module.exports = function(Group) {
 
+    Group.embedsMany(User, {
+        as: 'user', // default to the relation name - address
+        property: 'users' // default to addressItem
+    });
+
 };
