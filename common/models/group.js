95bab53fea3400395d28131024d73811536c809c
H3rN4n <desouza030689@gmail.com>
2016-12-13 12:51:30 -0300

    wip populate users in groups

--- common/models/group.js
+++ common/models/group.js
@@ -1,11 +1,25 @@
 'use strict';
-//var User = require('./user.js');
 
 module.exports = function(Group) {
 
-    // Group.embedsOne(User, {
-    //     as: 'user', // default to the relation name - address
-    //     property: 'users' // default to addressItem
-    // });
+//   Group.observe('loaded', function setDefaultUsername(ctx, next) {
+//     if (ctx.instance) {
+//       console.log(ctx.instance.users);
+//       var groupUsers = ctx.instance.users ? ctx.instance.users() : [];
+      
+//         for (var i = 0; i < groupUsers.length; i++) {
+//           var userId = groupUsers[i].userId;
+//           console.log(userId);
+//           ctx.Model.app.modules.user.findById(userId, function(err, user) {
+//             if (err) {
+//               return next(err);
+//             }
+//             ctx.instance.usersPopulated = Object.assign({}, groupUsers[i], user);
+//           });
+//         }
+      
+//     }
+//     next();
+//   });
 
 };
--- common/models/user.js
+++ common/models/user.js
@@ -1,10 +1,23 @@
 module.exports = function (user) {
 
   // Set the username to the users email address by default.
+
+  // Hooks
+  // access
+  // before save
+  // after save
+  // before delete
+  // after delete
+  // loaded
+  // persist
+
   user.observe('before save', function setDefaultUsername(ctx, next) {
     if (ctx.instance) {
       if(ctx.isNewInstance) {
         ctx.instance.username = ctx.instance.email;
+      }
+      if(ctx.instance.firstName && ctx.instance.lastName){
+        ctx.instance.fullName = ctx.instance.firstName + ' ' + ctx.instance.lastName;
       }
       ctx.instance.status = 'created';
       ctx.instance.created = Date.now();
