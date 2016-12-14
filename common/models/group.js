bae7a27e20ac4d69d8819fe8e54e796c6853a9c6
Daniel Geslin <dgeslin@belatrix.com>
2016-12-14 12:02:55 -0300

    groupUsers & warnings

--- common/models/group.js
+++ common/models/group.js
@@ -3,25 +3,29 @@
 module.exports = function (Group) {
 
     Group.observe('loaded', function setDefaultUsername(ctx, next) {
-        if(!ctx.instance){
+        if (!ctx.instance) {
             next();
             return;
         }
         var promise = new Promise(function (res, rej) {
             var groupUsers = ctx.instance.users ? ctx.instance.users() : [];
-
+            var query = { where: { or: [] } };
             if (!groupUsers || !groupUsers.length) res();
 
-            for (var i = 0; i < groupUsers.length; i++) {
-                var userId = groupUsers[i].userId;
-                (function (counter) {
-                    ctx.Model.app.models.user.findById(userId, function (err, user) {
-                        if (err) return;
-                        Object.assign(groupUsers[counter], user)
-                        if (counter === groupUsers.length - 1) res();
+            groupUsers.forEach(function (user) {
+                query.where.or.push({ id: user.userId });
+            });
+
+            ctx.Model.app.models.user.find(query, function (err, users) {
+                groupUsers.forEach(function (groupUser) {
+                    users.forEach(function (user) {
+                        if(groupUser.userId && user.id && groupUser.userId.id === user.id.id){
+                            Object.assign(groupUser, user);
+                        }
                     });
-                })(i);
-            }
+                });
+                res();
+            });
         });
         promise.then(next);
     });
